e62bb850b9ed861938a5a92e18b478c7297a0551
TINVO04 <tinvo.bh2018@gmail.com>
2024-08-26 14:38:00 +0700

    Bai Buổi 3

--- BaiTap02/BaiTap01/obj/Debug/net8.0/BaiTap01.AssemblyInfo.cs
+++ BaiTap02/BaiTap01/obj/Debug/net8.0/BaiTap01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaiTap01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a14022b9eb9501d6a81eb73ef482fbfa68639ae6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a93c4898d173098570886ae9cf071ccdd2c06830")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaiTap01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaiTap01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
