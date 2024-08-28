5d2922e055d7ca49133e2b935e34bf5572fdab86
TINVO04 <tinvo.bh2018@gmail.com>
2024-08-28 15:11:17 +0700

    Controller

--- BaiTap02B/BaiTapVeNha/obj/Debug/net8.0/BaiTapVeNha.AssemblyInfo.cs
+++ BaiTap02B/BaiTapVeNha/obj/Debug/net8.0/BaiTapVeNha.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaiTapVeNha")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a85bffb5ca5100c447d517b521065bc147dd4252")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65d071ad4754c7cbc82f51eb1ddbbe989d7b7a70")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaiTapVeNha")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaiTapVeNha")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
